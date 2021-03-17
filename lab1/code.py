8de8bafd5896b133363d51fcd842424dbf01bc56
samirkumar111 <53811273+samirkumar111@users.noreply.github.com>
2021-03-17 15:06:08 +0530

    Update code.py

--- lab1/code.py
+++ lab1/code.py
@@ -1,28 +1,34 @@
-import csv
-num_attributes = 6
-a = []
-print("\n The Given Training Data Set \n")
-with open('enjoysport.csv', 'r') as csvfile:
- reader = csv.reader(csvfile)
- for row in reader:
- a.append (row)
- print(row)
-print("\n The initial value of hypothesis: ")
-hypothesis = ['0'] * num_attributes
-print(hypothesis)
-for j in range(0,num_attributes):
- hypothesis[j] = a[0][j];
-print("\n Find S: Finding a Maximally Specific Hypothesis\n")
-for i in range(0,len(a)):
- if a[i][num_attributes]=='yes':
- for j in range(0,num_attributes):
- if a[i][j]!=hypothesis[j]:
- hypothesis[j]='?'
- else :
- hypothesis[j]= a[i][j]
- print(" For Training instance No:{0} the hypothesis is
-".format(i),hypothesis)
-
-print("\n The Maximally Specific Hypothesis for a given Training
-Examples :\n")
-print(hypothesis)
+import pandas as pd
+import numpy as np
+ 
+#to read the data in the csv file
+data = pd.read_csv("enjoysport.csv")
+print(data,"\n")
+ 
+#making an array of all the attributes
+d = np.array(data)[:,:-1]
+print("\n The attributes are: ",d)
+ 
+#segragating the target that has positive and negative examples
+target = np.array(data)[:,-1]
+print("\n The target is: ",target)
+ 
+#training function to implement find-s algorithm
+def train(c,t):
+    for i, val in enumerate(t):
+        if val == "Yes":
+            specific_hypothesis = c[i].copy()
+            break
+             
+    for i, val in enumerate(c):
+        if t[i] == "Yes":
+            for x in range(len(specific_hypothesis)):
+                if val[x] != specific_hypothesis[x]:
+                    specific_hypothesis[x] = '?'
+                else:
+                    pass
+                 
+    return specific_hypothesis
+ 
+#obtaining the final hypothesis
+print("\n The final hypothesis is:",train(d,target))
