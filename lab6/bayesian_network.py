486a0231a3d6b218e4a54c0e1cfccdb817174785
samirkumar111 <53811273+samirkumar111@users.noreply.github.com>
2021-05-07 08:35:31 +0530

    Create bayesian_network.py

---
+++ lab6/bayesian_network.py
@@ -0,0 +1,64 @@
+from pgmpy.models import BayesianModel
+from pgmpy.factors.discrete import TabularCPD
+from pgmpy.inference import VariableElimination
+
+#Define a Structure with nodes and edges
+cancer_model = BayesianModel([('Pollution', 'Cancer'), 
+                              ('Smoker', 'Cancer'),
+                              ('Cancer', 'Xray'),
+                              ('Cancer', 'Dyspnoea')])
+print('Bayesian network nodes:')
+print('\t', cancer_model.nodes())
+print('Bayesian network edges:')
+print('\t', cancer_model.edges())
+
+#Creation of Conditional Probability Table
+cpd_poll = TabularCPD(variable='Pollution', variable_card=2,
+                      values=[[0.9], [0.1]])
+cpd_smoke = TabularCPD(variable='Smoker', variable_card=2,
+                       values=[[0.3], [0.7]])
+cpd_cancer = TabularCPD(variable='Cancer', variable_card=2,
+                        values=[[0.03, 0.05, 0.001, 0.02],
+                                [0.97, 0.95, 0.999, 0.98]],
+                        evidence=['Smoker', 'Pollution'],
+                        evidence_card=[2, 2])
+cpd_xray = TabularCPD(variable='Xray', variable_card=2,
+                      values=[[0.9, 0.2], [0.1, 0.8]],
+                      evidence=['Cancer'], evidence_card=[2])
+cpd_dysp = TabularCPD(variable='Dyspnoea', variable_card=2,
+                      values=[[0.65, 0.3], [0.35, 0.7]],
+                      evidence=['Cancer'], evidence_card=[2])
+
+# Associating the parameters with the model structure.
+cancer_model.add_cpds(cpd_poll, cpd_smoke, cpd_cancer, cpd_xray, cpd_dysp)
+print('Model generated bt adding conditional probability distribution(cpds)')
+
+# Checking if the cpds are valid for the model.
+print('Checking for Correctness of model:', end='')
+print(cancer_model.check_model())
+
+
+'''print('All local dependencies are as follows')
+cancer_model.get_independencies()
+'''
+
+print('Displaying CPDs')
+print(cancer_model.get_cpds('Pollution'))
+print(cancer_model.get_cpds('Smoker'))
+print(cancer_model.get_cpds('Cancer'))
+print(cancer_model.get_cpds('Xray'))
+print(cancer_model.get_cpds('Dyspnoea'))
+
+#Inferencing with Bayesian Network
+#Computing the probability of Cancer given smoke
+
+cancer_infer = VariableElimination(cancer_model)
+print('\nInferencing with Bayesian Network')
+
+print('\nProbability of Cancer given Smoker')
+q = cancer_infer.query(variables=['Cancer'], evidence={'Smoker': 1})
+print(q)
+
+print('\nProbability of Cancer given Smoker, Pollution')
+q = cancer_infer.query(variables=['Cancer'], evidence={'Smoker': 1,'Pollution': 1})
+print(q)
